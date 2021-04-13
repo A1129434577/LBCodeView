f5addb8c33afc08a99f22b99d166dc453b8b01d4
刘彬 <1129434577@qq.com>
2021-04-13 17:25:30 +0800

    优化hiddenTextField的位置

--- LBCodeView/LBCodeView.m
+++ LBCodeView/LBCodeView.m
@@ -38,10 +38,10 @@
             [self addSubview:codeShowButton];
             [(NSMutableArray *)_codeShowButtons addObject:codeShowButton];
         }
+        _hiddenTextField.frame = CGRectMake(0, CGRectGetMaxY(codeShowButton.frame), CGRectGetWidth(frame), 0);
         
         _cursorView = [[UIView alloc] initWithFrame:CGRectMake((showButtonSide-1.5)/2, (showButtonSide-codeShowButton.titleLabel.font.lineHeight)/2, 1.5, codeShowButton.titleLabel.font.lineHeight)];
         _cursorView.backgroundColor = [UIColor magentaColor];
-        
     }
     return self;
 }
